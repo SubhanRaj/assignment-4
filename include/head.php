9455f574b14f7582dd820e55c5e3a0f6c11c1998
Subhan Raj <shubhanraj2002@gmail.com>
2022-05-30 05:56:26 +0530

    head

--- include/head.php
+++ include/head.php
@@ -9,5 +9,5 @@
     <link rel="icon" type="image/png" sizes="32x32" href="./favicon/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="./favicon/favicon-16x16.png">
     <link rel="manifest" href="./favicon/site.webmanifest">
-    <title>Webtech Assignment</title>
+    <title>Assignment-4</title>
 </head>
